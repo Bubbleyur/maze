47eefd612684ccec901d9464e0f9dcc6f56ca27c
David-disini <christomoniaga@gmail.com>
2026-05-15 10:29:10 +0700

    backtracking game

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.sql.Time;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class App extends JPanel{
-
+    boolean gameSelesai = false;
     public App() {
 
         Assets.load();
@@ -14,6 +20,16 @@ public class App extends JPanel{
         super.paintComponent(g);
 
         GameData.drawMap(g);
+        GameData.drawPlayer(g);
+        // tampilan game selesai
+        if(gameSelesai){
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.YELLOW);
+            g.setFont(new Font("Arial", Font.BOLD, 60));
+            g.drawString("Congrats", 250, 250);
+        }
+
     }
 
     static int[][] map = {
@@ -31,7 +47,7 @@ public class App extends JPanel{
         {0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,0,1,0},
         {0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,1,0},
         {0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0},
-        {0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,3,0},
         {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
     };
 
@@ -51,7 +67,35 @@ public class App extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
-        
-        // tes
+
+        GameData.playerX = 1;
+        GameData.playerY = 1;
+
+        panel.mulaiPencarian();
+
+    }
+
+    public void mulaiPencarian(){
+        boolean ketemu = Pathfind.jalanBenar(GameData.playerX, GameData.playerY);
+
+        if(ketemu){
+            Timer time = new Timer(500, e ->{ //0,5 detik
+                if(!Pathfind.rute.isEmpty()){
+                    Point nextMove = Pathfind.rute.remove(0); // cara kerjanya simpan nilai dari indeks 0 ke nextMove, hapus indeks 0 biar else nya jalan
+                    GameData.playerX = nextMove.x; // nyimpan indeks 0 (X)
+                    GameData.playerY = nextMove.y; // nyimpan indeks 0 (Y)
+                    repaint();
+                }
+                else{
+                    ((Timer) e.getSource()).stop();
+                    gameSelesai = true;
+                    repaint();
+                }
+            });
+            time.start();
+        }
+        else{
+            System.out.println("Jalan buntu");
+        }
     }
 }
--- src/Pathfind.java
+++ src/Pathfind.java
@@ -1,3 +1,41 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
 public class Pathfind {
-    // backtracking functions disini semua
+    static ArrayList<Point> rute = new ArrayList<>();
+    public static boolean jalanBenar(int startX, int startY){
+        GameData.resetVisited();
+        rute.clear();
+        return solve(startX, startY);
+    }
+
+    public static boolean solve(int x, int y){
+        // BASECASE
+        if(GameData.map[y][x] == GameData.EXIT){ // EXIT = 3
+            System.out.println("Ketemu jalan keluar");
+            rute.add(0,new Point(x,y)); // cara cepat dari rute.add(0, int[]{x,y});
+            return true;
+        }
+        // penanda kalo kordinat x,y sudah pernah dilewati
+        GameData.visited[y][x] = true;
+
+        // pengecekan atas,bawah,kanan,samping
+        for(int i=0; i<4; i++){
+            int nextX = x + GameData.dx[i];
+            int nxtY = y + GameData.dy[i];
+            // pengecekan apakah itu batas frame atau wall
+            if(GameData.canMove(nextX, nxtY)){
+                if(solve(nextX, nxtY)){ // rekursi
+                    rute.add(0 ,new Point(x,y));
+                    return true;
+                }
+            }
+        }
+
+        // mereset penanda
+        GameData.visited[y][x] = false;
+
+        // mundur satu langkah
+        return false;
+    }
 }
